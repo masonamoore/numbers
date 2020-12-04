802ec4486ee92778ab24ee757b4547f2abebdf7e
Mason Moore <masonmoore@Masons-MacBook-Air.local>
2020-12-03 22:57:57 -0800

    Adds numbers.sh and README.md

---
+++ numbers.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#! numbers.sh
+#Mason Moore
+echo "Please enter a positive integer:"
+read X
+N=1
+while [ $N -le $X ]
+do
+	echo $N
+	if [ $((N%2)) -eq 0 ]
+	then
+		echo "Even"
+	else
+		echo "Odd"
+	fi
+	((N++))
+done
